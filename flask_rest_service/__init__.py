801ec4048dc259f37d5a0eb62e3a559737e859f8
Danni40 <dlewis2@live.maryville.edu>
2018-12-11 20:02:42 -0600

    preparing for deployment

---
+++ flask_rest_service/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+from flask import make_response
+from bson.json_util import dumps
+
+#MONGO_URL = os.environ.get('MONGO_URL')
+
+#if not MONGO_URL:
+    #MONGO_URL = 'mongodb://127.0.0.1:27017/test'
+
+app = Flask(__name__, static_url_path = "", static_folder = "static")
+app.config['DEBUG'] = True
+app.config['STATIC_FOLDER'] = '/static'
+
+#app.config['MONGO_URI'] = MONGO_URL
+app.config['MONGO_DBNAME'] = 'wod_bucket'
+app.config['MONGO_URI'] = 'mongodb://danni40:Tiagdtd39#(01@ds131954.mlab.com:31954/wod_bucket'
+mongo = PyMongo(app)
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
+api = Api(app)
+api.representations = DEFAULT_REPRESENTATIONS
+
+import flask_rest_service.main
--- flask_rest_service/main.py
+++ flask_rest_service/main.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, redirect, render_template, session, flash, url_for
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 from random import randint
 import traceback
 import datetime
 import cgi
-
-#Connect to MongoDB - Note: Change connection string as needed
-client = MongoClient('mongodb://127.0.0.1:27017/test')
-db=client.wod_test
-
-#initiate appe and static folders for static artifacts such as pics and stylesheets
-app = Flask(__name__, static_url_path = "", static_folder = "static")
-app.config['DEBUG'] = True
-app.config['STATIC_FOLDER'] = '/static'
+import json
+from flask import request, abort
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+from flask_rest_service import app, api, mongo
+from bson.objectid import ObjectId
 
 class User:
 
@@ -43,9 +39,9 @@ def login():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        user_count=db.register.find( {'email': email } ).count()
+        user_count=mongo.db.register.find( {'email': email } ).count()
         if user_count == 1:
-            user = db.register.find_one({'email':email})
+            user = mongo.db.register.find_one({'email':email})
 
             if password == user.get('password'):
                 session['user'] = username
@@ -62,7 +58,7 @@ def update():
     exercise1_error=''
     exercise2_error=''
     exercise3_error=''
-    username = db.register.find_one({'username': session['user']})
+    username = mongo.db.register.find_one({'username': session['user']})
     try:
         if request.method == 'POST':
             
@@ -89,16 +85,17 @@ def update():
                 exercise3_error='Exercise #3 not entered'
 
             if not exercise1_error and not exercise2_error and not exercise3_error:
-                result=db.workouts.insert_one(workout)
-                workouts = db.workouts.find({'username':username})
-                workout_id = db.workouts.find_one({'workouts._id': result.inserted_id})
+                result=mongo.db.workouts.insert_one(workout)
+                workouts = mongo.db.workouts.find({'username':username})
+                workout_id = mongo.db.workouts.find_one({'workouts._id': result.inserted_id})
                 return redirect("/")
 
             else:
-                return redirect('/', exercise1_error=exercise1_error, exercise2_error=exercise2_error, exercise3_error=exercise3_error)
+                flash('Please enter some exercises' + username)
+                return render_template('index.html')
             
-        else:
-            render_template('index.html')
+        #else:
+            #return render_template('index.html')
 
     except Exception:
             traceback.print_exc()
@@ -117,11 +114,11 @@ def signup():
         if not is_email(email):
             flash('zoiks! "' + email + '" does not seem like an email address')
             return redirect('/signup')
-        existing_user = db.register.count( {'email': email } )
+        existing_user = mongo.db.register.count( {'email': email } )
         if existing_user > 0:
             flash('yikes! "' + email + '" is already taken "')
             return redirect('/signup')
-        username_db_count = db.register.count( { 'username' : username} )
+        username_db_count = mongo.db.register.count( { 'username' : username} )
         if username_db_count > 0:
             flash('yikes! "' + username + '" is already taken "')
             return redirect('/signup')     
@@ -141,7 +138,7 @@ def signup():
         'email' : email,
         'password' : password,
         }
-        result=db.register.insert_one(user)
+        result=mongo.db.register.insert_one(user)
         session['user'] = username
         session['logged_in'] = True
         return redirect('/')
@@ -172,10 +169,10 @@ def index():
     if ('user' in session):
         #get workouts for the specific user
         username = session['user']
-        userWorkoutsCount = db.workouts.count({'username': username})
-        userWorkouts = db.workouts.find({'username': username})
+        userWorkoutsCount = mongo.db.workouts.count({'username': username})
+        userWorkouts = mongo.db.workouts.find({'username': username})
         now = datetime.datetime.now()
-        lastEntry = db.workouts.find({'username': username}).limit(1).sort("_id",-1)
+        lastEntry = mongo.db.workouts.find({'username': username}).limit(1).sort("_id",-1)
         
         #populate the recommended workout
         cardio = ['Walk','Bike','Run', 'Skate', 'Swim','Jump Rope','Row', 'Stair Climber','Sprint', 'Elliptical','Spike Ball','Interval Run', 'Ergo']
@@ -188,6 +185,7 @@ def index():
         exercise2_num =  str(randint(12, 15)) + ' reps for ' + str(randint(1, 3)) + ' sets'
         exercise3_num =  str(randint(3, 5)) + ' minutes'
         return render_template('index.html', now=now.date(), userWorkoutsCount=userWorkoutsCount, userWorkouts=userWorkouts, username=username, lastEntry=lastEntry, exercise1=exercise1, exercise2=exercise2, exercise3=exercise3, exercise1_num=exercise1_num, exercise2_num=exercise2_num, exercise3_num=exercise3_num)
+    #C:\Users\the_c\OneDrive\Documents\691-Capstone\WODBucket\WODBucket\templates\index.html
     else:
         now = datetime.datetime.now()
         return render_template('index.html', now=now.date())
@@ -197,24 +195,24 @@ def index():
 def workout():
     if not request.args:
         username = session['user']
-        userWorkouts = db.workouts.find({'username': username})
-        [i for i in db.workouts.find({"username": username})]
-        return render_template("workouts.html", i=i, username=username, userWorkouts=userWorkouts)
+        userWorkouts = mongo.db.workouts.find({'username': username})
+        [i for i in mongo.db.workouts.find({"username": username})]
+        return render_template("workouts.html", username=username, userWorkouts=userWorkouts)
     
     if request.args.get('id'):
         username = session['user']
         _id = "ObjectId('" + request.args.get('id') + "')"
         workout_id = request.args.get('id')
-        userWorkoutsCount = db.workouts.count({'username': username})
-        userWorkouts = db.workouts.find({'username': username})
-        workout = db.workouts.find_one({'_id': _id})
-        [i for i in db.workouts.find({"_id": ObjectId(request.args.get('id'))})]
+        userWorkoutsCount = mongo.db.workouts.count({'username': username})
+        userWorkouts = mongo.db.workouts.find({'username': username})
+        workout = mongo.db.workouts.find_one({'_id': _id})
+        i = mongo.db.workouts.find({"_id": ObjectId(request.args.get('id'))})
         return render_template("workout.html", i=i, workout=workout, _id=_id, userWorkoutsCount=userWorkoutsCount, userWorkouts=userWorkouts, username=username)
 
 #have a page for all workouts to show
 @app.route("/workouts", methods=['GET'])
 def workouts():
-    return render_template("workouts.html", i=i, username=username, userWorkouts=userWorkouts)
+    return render_template("workouts.html", username=username, userWorkouts=userWorkouts)
 
 #allow only certain pages to be accessible when not logged in
 endpoints_without_login = ['login', 'signup','index', 'static', 'images', 'stylesheets']
